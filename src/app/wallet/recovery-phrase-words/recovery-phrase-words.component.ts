a43b927475b327d56b4f131597765a9945c3f73a
gohar1009 <gohar.avetisyan10@gmail.com>
2019-04-19 19:06:08 +0400

    recovery page

--- src/app/wallet/recovery-phrase-words/recovery-phrase-words.component.ts
+++ src/app/wallet/recovery-phrase-words/recovery-phrase-words.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Router } from '@angular/router';
+import { AccountService } from '../../core/services/account.service';
+import { FormControl } from '@angular/forms';
 
 
 @Component({
@@ -10,16 +12,44 @@ import { Router } from '@angular/router';
 })
 export class RecoveryPhraseWordsComponent implements OnInit{
   numberArray = [];
-	constructor(public router:Router, public dialog: MatDialog) {
+  brainKey;
+  word1:FormControl = new FormControl();
+  word2:FormControl = new FormControl();
+  word3:FormControl = new FormControl();
+  word4:FormControl = new FormControl();
+
+	constructor(private accountService:AccountService, public router:Router, public dialog: MatDialog) {
 
   }
   ngOnInit(){
     this.generateNumbers();
+    this.brainKey = this.accountService.decryptedBrainKey;
   }
 
   generateNumbers(){
+    let push = true;
     for(let i=0; i<=3 ;i++){
-      this.numberArray.push(Math.floor(Math.random()*12+1))
+      let random = Math.floor(Math.random()*12+1);
+      for(let j =0; j<this.numberArray.length;j++){
+        if(random == this.numberArray[j]){
+          push = false;
+        }
+      }
+      if(push){
+        this.numberArray.push(random);
+      }
+    }
+  }
+
+  finish(){
+    let brainKeyArray = this.brainKey.split(' ');
+    console.log(brainKeyArray);
+    if(brainKeyArray[this.numberArray[0]] == this.word1.value && 
+      brainKeyArray[this.numberArray[1]] == this.word2.value && 
+      brainKeyArray[this.numberArray[2]] == this.word3.value && 
+      brainKeyArray[this.numberArray[3]] == this.word4.value 
+    ){
+      console.log(true);
     }
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,5 @@ export const environment = {
   production: false,
   backend: "http://192.168.20.111:8010",
   NODE_URL: "https://:15eb3124a4d546a2b1c7b6b24bad5f3d@ropsten.infura.io/v3/50c327479b344b39a3f34c5344d86f2e",
-
+  eveg_contract_address:''
 };
