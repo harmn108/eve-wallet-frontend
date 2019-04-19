6e38fcd32c32322da352ab374cea234ea53840ff
gohar1009 <gohar.avetisyan10@gmail.com>
2019-04-19 19:57:53 +0400

    user auth finished

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  backend: "",
+  backend: "http://192.168.20.111:8010",
+  NODE_URL: "https://:15eb3124a4d546a2b1c7b6b24bad5f3d@ropsten.infura.io/v3/50c327479b344b39a3f34c5344d86f2e",
   eveg_contract_address:"0xe8ef7f2634326db7003d97a14e567bb368d63586",
   eveo_contract_address:"0x772586f72229495a7e06ae831c83449db3d1d5cb"
 };
